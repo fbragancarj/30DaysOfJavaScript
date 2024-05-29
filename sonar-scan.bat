46be6ed093098bc261c3c13ef1ec0a2ebcbf6bb8
Fabiano Braganca <fabraganca@mc.pt>
2024-05-29 20:33:25 +0100

    Update Sonar Scan

--- sonar-scan.bat
+++ sonar-scan.bat
@@ -20,7 +20,7 @@
 
 :main
 @echo "Scanning branch main"
-sonar-scanner.bat -D"sonar.projectKey=30DaysOfJavaScript" -D"sonar.sources=." -D"sonar.host.url=http://127.0.0.1:9000" -D"sonar.token=sqp_23150addfe86ee7e5c02ec2b5692abd86b6e82ad"
+sonar-scanner.bat -D"sonar.projectKey=30DaysOfJavaScript" -D"sonar.sources=." -D"sonar.host.url=http://127.0.0.1:9000" -D"sonar.token=sqp_9ae5891b9fe88921fc8b927189693d96afb10e52"
 @goto end
 
 :others
